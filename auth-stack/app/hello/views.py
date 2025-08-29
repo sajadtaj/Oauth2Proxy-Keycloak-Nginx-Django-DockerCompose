2806f5741886391a904ccd14c2e81cfb89f5a94d
sajadtaj <sajadtajco@gmail.com>
2025-08-29 13:23:03 +0330

    complete project|complete phase 3

--- auth-stack/app/hello/views.py
+++ auth-stack/app/hello/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+# auth-stack/app/core/views.py
 from django.http import HttpResponse
+from django.views.decorators.http import require_GET
 
+@require_GET
 def public_view(request):
     return HttpResponse("Hello Public", content_type="text/plain; charset=utf-8")
 
+@require_GET
 def private_view(request):
-    return HttpResponse("Hello Private", content_type="text/plain; charset=utf-8")
+    # هدرها در WSGI با پیشوند HTTP_ در دسترس‌اند
+    user  = request.META.get("HTTP_X_USER") or request.META.get("HTTP_X_AUTH_REQUEST_USER") or "Unknown"
+    email = request.META.get("HTTP_X_EMAIL") or request.META.get("HTTP_X_AUTH_REQUEST_EMAIL") or ""
+    # نمایش مینیمال (Plain Text)
+    if email:
+        msg = f"Hello Private, User:{user} , Email ({email})"
+    else:
+        msg = f"Hello Private, {user}"
+    return HttpResponse(msg, content_type="text/plain; charset=utf-8")
